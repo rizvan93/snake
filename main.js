ee621207bdf0a902227a9e1370eaf7639ce11ed8
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-02-15 10:38:18 +0800

    changed js to initialize table based on model grid

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const HEIGHT = 20;
-const WIDTH = 30;
+const HEIGHT = 3;
+const WIDTH = 5;
 const STARTLEN = 4;
 const STARTLOC = [HEIGHT / 2];
 const app = {};
@@ -8,21 +8,27 @@ const app = {};
 const table = document.querySelector("table");
 
 const addBoardBorders = (box, x, y) => {
-  if (i === 0) box.style.borderTopColor = "black";
-  else if (i === HEIGHT - 1) box.style.borderBottomColor = "black";
-  if (j === 0) box.style.borderLeftColor = "black";
-  if (j === WIDTH - 1) box.style.borderRightColor = "black";
+  if (x === 0) {
+    box.style.borderTopColor = "black";
+  } else if (x === HEIGHT - 1) {
+    box.style.borderBottomColor = "black";
+  }
+  if (y === 0) {
+    box.style.borderLeftColor = "black";
+  } else if (y === WIDTH - 1) {
+    box.style.borderRightColor = "black";
+  }
   return box;
 };
 
 const initializeTable = () => {
-  for (i = 0; i < HEIGHT; i++) {
+  for (i in app.grid) {
     const row = document.createElement("tr");
     row.setAttribute("data-row", i);
-    for (j = 0; j < WIDTH; j++) {
+    for (j in app.grid[i]) {
       const box = document.createElement("td");
-      row.setAttribute("data-col", j);
-      row.append(addBoardBorders(box));
+      box.setAttribute("data-col", j);
+      row.append(addBoardBorders(box, parseInt(i), parseInt(j)));
     }
     table.append(row);
   }
